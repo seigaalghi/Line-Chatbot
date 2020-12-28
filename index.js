53e27e3a5f20ffd8f8f813f0a07c2193841461a9
seiga al ghifari <seigaalghi@gmail.com>
2020-12-28 16:47:45 +0700

    fix

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ require('dotenv').config();
 const line = require('@line/bot-sdk');
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors');
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.CHANNEL_SECRET,
 };
 const client = new line.Client(config);
 
+app.use(cors());
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello Biatch' });
+  res.status(200).json({ message: 'Hello' });
 });
 
 app.post('/webhook', line.middleware(config), (req, res) => {
@@ -28,5 +33,4 @@ function mainProgram(event) {
   return client.replyMessage(event.replyToken, { type: 'text', text: 'Hello, world' }); //balas dengan pesan "Hello, world"
 }
 
-const port = 5000 || process.env.PORT;
-app.listen(port, () => console.log('Server Started at port 5000'));
+app.listen(port, () => console.log(`Server Started at port ${port}`));
