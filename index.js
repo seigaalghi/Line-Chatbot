db01aad3ba339ac8c761b33c575646e7dddd8d15
seiga al ghifari <seigaalghi@gmail.com>
2020-12-28 16:36:16 +0700

    sa

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.CHANNEL_SECRET,
 };
-console.log(process.env.CHANNEL_ACCESS_TOKEN);
 const client = new line.Client(config);
 
 app.get('/', (req, res) => {
-  res.sendStatus(404);
+  res.status(200).json({ message: 'Hello Biatch' });
 });
+
 app.post('/webhook', line.middleware(config), (req, res) => {
   Promise.all(req.body.events.map(mainProgram))
     .then((result) => res.json(result))
@@ -19,6 +19,14 @@ app.post('/webhook', line.middleware(config), (req, res) => {
       console.error(`Promise error ${error}`);
     });
 });
+
+function mainProgram(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    //jika user tidak mengirimkan pesan berupa teks (bukan gambar, lokasi, atau sejenisnya)
+    return Promise.resolve(null); //abaikan pesan
+  }
+  return client.replyMessage(event.replyToken, { type: 'text', text: 'Hello, world' }); //balas dengan pesan "Hello, world"
+}
 
 const port = 5000 || process.env.PORT;
 app.listen(port, () => console.log('Server Started at port 5000'));
